72c27da7ef19397059886a3973a9e954ac7ac76d
Mateo Gonzalez Amortegui <mateogonza.2000@hotmail.com>
2023-12-26 19:36:18 -0500

    🎉 finish navbar (components, pages, theme): Folder Components common, UI, icons. Pages profile, maps, pets. Theme darkmode y daisyui config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,9 +6,19 @@ const config: Config = {
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
+    darkMode: "class",
     plugins: [require("daisyui")],
     daisyui: {
-        themes: ["forest", "fantasy"],
+        themes: [
+            {
+                "my-theme": {
+                    primary: "#7e22ce",
+                    secondary: "#2563eb",
+                    accent: "#f97316",
+                    neutral: "#9ca3af",
+                },
+            },
+        ],
     },
 }
 export default config
