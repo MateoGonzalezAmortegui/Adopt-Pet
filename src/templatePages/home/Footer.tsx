f4a8a6a1acf6e42b9232216da3a6ce7e048fca5b
Mateo Gonzalez Amortegui <mateogonza.2000@hotmail.com>
2024-01-12 19:46:01 -0500

    🛠️correction styles folder tailwind

--- src/templatePages/home/Footer.tsx
+++ src/templatePages/home/Footer.tsx
@@ -4,32 +4,34 @@ import Image from "next/image"
 import Link from "next/link"
 export const Footer = () => {
     return (
-        <footer className="footer items-center p-4 bg-neutral text-neutral-content">
-            <aside className="items-center grid-flow-col">
-                <Image
-                    src="/logo.png"
-                    alt="hero"
-                    className="max-w-sm rounded-lg shadow-2xl"
-                    width="150"
-                    height="133"
-                />
-                <p>Mateo Gonzalez Amortegui © 2024</p>
-            </aside>
-            <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-                <Link
-                    href="https://github.com/MateoGonzalezAmortegui"
-                    target="_blank"
-                >
-                    <GitHubIcon className="fill-current text-white w-6 h-6" />
-                </Link>
+        <>
+            <footer className="footer items-center p-4 bg-neutral text-neutral-content pb-24 md:pb-4">
+                <aside className="items-center grid-flow-col">
+                    <Image
+                        src="/logo.png"
+                        alt="hero"
+                        className="max-w-sm rounded-lg shadow-2xl"
+                        width="150"
+                        height="133"
+                    />
+                    <p>Mateo Gonzalez Amortegui © 2024</p>
+                </aside>
+                <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+                    <Link
+                        href="https://github.com/MateoGonzalezAmortegui"
+                        target="_blank"
+                    >
+                        <GitHubIcon className="fill-current text-white w-6 h-6" />
+                    </Link>
 
-                <Link
-                    href="https://www.linkedin.com/in/mateo-gonzalez-amortegui/"
-                    target="_blank"
-                >
-                    <LinkedinIcon className="fill-current text-white w-6 h-6" />
-                </Link>
-            </nav>
-        </footer>
+                    <Link
+                        href="https://www.linkedin.com/in/mateo-gonzalez-amortegui/"
+                        target="_blank"
+                    >
+                        <LinkedinIcon className="fill-current text-white w-6 h-6" />
+                    </Link>
+                </nav>
+            </footer>
+        </>
     )
 }
